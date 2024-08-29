3ed5bd08fa65c12613a5090391b79d86d699cd64
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-08-29 21:21:07 +0500

    Update Doubly-Circular-Basics.cpp

--- Data-Structures-Algorithms/Linked Lists/3. Circular Linked Lists/2. Doubly Circular Linked List/Doubly-Circular-Basics.cpp
+++ Data-Structures-Algorithms/Linked Lists/3. Circular Linked Lists/2. Doubly Circular Linked List/Doubly-Circular-Basics.cpp
@@ -6,7 +6,7 @@ class Node {
 
 public:
 
-	int data;
+	float data;
 	Node* prev, * next;
 
 	// Constructor
@@ -25,6 +25,11 @@ void forward_traverse_DC_ll(Node* head) {
 	if (head == NULL) {
 
 		cout << "Linked List is empty" << endl;
+		return;
+	}
+	else if (head->next == NULL) {
+
+		cout << head->data << endl;
 		return;
 	}
 
@@ -46,6 +51,11 @@ void reverse_traverse_DC_ll(Node* head) {
 		cout << "Linked List is empty" << endl;
 		return;
 	}
+	else if (head->next == NULL) {
+
+		cout << head->data << endl;
+		return;
+	}
 
 	Node* iterNode = head;
 
@@ -58,16 +68,27 @@ void reverse_traverse_DC_ll(Node* head) {
 	cout << endl;
 }
 
-void insert_at_head_DC_ll(Node*& head, int data) {
+void insert_at_head_DC_ll(Node*& head, float data) {
 
 	if (head == NULL) {
 
 		cout << "Linked List is empty" << endl;
 		return;
 	}
-	else {
 
-		Node* n = new Node(data);
+	Node* n = new Node(data);
+
+	if (head->next == NULL){
+
+		
+		head->prev = n;
+		head->next = n;
+		n->prev = head;
+		n->next = head;
+
+		head = n;
+	}
+	else {
 
 		n->next = head;
 		n->prev = head->prev;
@@ -75,10 +96,12 @@ void insert_at_head_DC_ll(Node*& head, int data) {
 		head->prev = n;
 
 		head = n;
+
+		return;
 	}
 }
 
-void insert_after_DC_ll(Node*& head, int data, int element) {
+void insert_after_DC_ll(Node*& head, float data, float element) {
 
 	Node* n = new Node(data);
 
@@ -116,7 +139,7 @@ void insert_after_DC_ll(Node*& head, int data, int element) {
 	} while (iterNode != head);
 }
 
-void deletion_DC_ll(Node*& head, int element) {
+void deletion_DC_ll(Node*& head, float element) {
 
 	if (head == NULL) {
 
@@ -127,17 +150,46 @@ void deletion_DC_ll(Node*& head, int element) {
 	
 	if (head->data == element) {
 
-		iterNode->next->prev = iterNode->prev;
-		iterNode->prev->next = iterNode->next;
+		if (head->next == NULL) {
 
-		head = head->next;
-		return;
+			cout << "This is last element in the Linked List.\n" <<
+				"Do you want to delete this NODE  (y/n) : ";
+
+			char option;
+
+			cin >> option;
+
+			if (option == 'y' || option == 'Y') {
+
+				cout << "NODE having " << head->data << " value DELETED." << endl;
+				delete head;
+				head = NULL;
+				return;
+			}
+			else if (option == 'n' || option == 'Y') {
+
+				cout << "Returning without deleting NODE." << endl;
+
+				return;
+			}
+		}
+		else {
+
+			cout << "NODE having " << head->data << " value DELETED." << endl;
+
+			iterNode->next->prev = iterNode->prev;
+			iterNode->prev->next = iterNode->next;
+			head = head->next;
+			return;
+		}
 	}
 
 
 	do {
 
 		if (iterNode->data == element) {
+
+			cout << "NODE having " << iterNode->data << " value DELETED." << endl;
 
 			iterNode->next->prev = iterNode->prev;
 			iterNode->prev->next = iterNode->next;
@@ -152,16 +204,83 @@ void deletion_DC_ll(Node*& head, int element) {
 
 int doubly_circular_ll_basic() {
 
-	Node* head = new Node(10);
-	insert_after_DC_ll(head, 20, 10);
-	insert_after_DC_ll(head, 30, 20);
-	insert_at_head_DC_ll(head, 0);
 
-	forward_traverse_DC_ll(head);
+	cout << "Enter First Node value : ";
 
-	deletion_DC_ll(head, 0);
+	float first_value;
 
-	forward_traverse_DC_ll(head);
+	cin >> first_value;
+
+	Node* head = new Node(first_value);
+	
+	float data, element;
+	int option;
+	bool condition = true;
+
+	while (condition) {
+
+
+		cout << "1. Insert at head.\n" <<
+				"2. Insert after number.\n" <<
+				"3. Node Deletion.\n" <<
+				"4. Forward Traverse.\n" <<
+				"5. Backward Traverse.\n" <<
+				"6. Exit.\n" << endl;
+
+
+		cout << "Enter the function you want to perform : ";
+
+		cin >> option;
+
+		switch (option)
+		{
+		case 1:
+
+			cout << "Enter the number you want to store: ";
+			cin >> data;
+			insert_at_head_DC_ll(head, data);
+			break;
+
+		case 2:
+
+			cout << "Enter the number you want to store: ";
+			cin >> data;
+
+			cout << "\nAfter which number you want to add given number (above): ";
+			cin >> element;
+
+			insert_after_DC_ll(head, data, element);
+			break;
+
+		case 3:
+
+			cout << "Enter the element you want to delete: ";
+			cin >> element;
+			deletion_DC_ll(head, element);
+			break;
+
+		case 4:
+
+			forward_traverse_DC_ll(head);
+			break;
+
+		case 5:
+
+			reverse_traverse_DC_ll(head);
+			break;
+
+		case 6:
+
+			condition = false;
+			break;
+
+		default:
+			
+			cout << "Enter correct function" << endl;
+			break;
+		}
+	}
+
 
 	return 0;
 }
