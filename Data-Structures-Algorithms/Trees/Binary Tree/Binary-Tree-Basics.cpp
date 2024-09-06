2ca9e7f0da862a90a16d2bd3c30600739542a205
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-09-06 11:25:53 +0500

    Update Binary-Tree-Basics.cpp

--- Data-Structures-Algorithms/Trees/Binary Tree/Binary-Tree-Basics.cpp
+++ Data-Structures-Algorithms/Trees/Binary Tree/Binary-Tree-Basics.cpp
@@ -19,7 +19,7 @@ public:
 	}
 };
 
-Node* build_tree(Node* root) {
+Node* build_tree_recursive(Node* root) {
 
 	cout << "Enter the data : ";
 	int data;
@@ -27,15 +27,15 @@ Node* build_tree(Node* root) {
 
 	if (data == -1) {
 
-		return NULL;
+		return nullptr;
 	}
 
 	root = new Node(data);
-	
-	cout << "Enter data for inserting in the left " << data << endl;
-	root->left = build_tree(root->left);
-	cout << "Enter data for inserting in the right " << data << endl;
-	root->right = build_tree(root->right);
+
+	cout << "Enter the data for the left of " << data << endl;
+	root->left = build_tree_recursive(root->left);
+	cout << "Enter the data for the right of " << data << endl;
+	root->right = build_tree_recursive(root->right);
 
 	return root;
 }
@@ -53,7 +53,7 @@ void level_order_traversal(Node* root) {
 
 		if (temp == nullptr) {
 
-			cout << "\t" << endl;
+			cout << endl;
 			if (!q.empty()) {
 
 				q.push(nullptr);
@@ -73,9 +73,104 @@ void level_order_traversal(Node* root) {
 	}
 }
 
+void build_tree(Node* &root) {
+
+	cout << "\n\n\nEnter the data : ";
+	int data;
+	cin >> data;
+
+	bool condition = true;
+
+	Node* temp = root;
+
+	while (condition) {
+
+		cout << "\nCurrent Node value is " << temp->data << endl;
+		cout << "\nChoose side\n" <<
+			"1. Left.\n" <<
+			"2. Right.\n" << endl;
+
+		Node* newNode = new Node(data);
+
+		int option, choice;
+		cin >> option;
+
+		switch (option)
+		{
+		case 1:
+
+			cout << "\n\nDo you want to add on the left of " << temp->data << " (1/0)" << endl;
+			cin >> choice;
+
+			if (choice == 1) {
+
+				temp->left = newNode;
+				return;
+			}
+			else if (choice == 0) {
+
+				if (temp->left == NULL) {
+
+					cout << "\nTree ends here" << endl;
+				}
+				else {
+
+					temp = temp->left;
+					cout << "\nMoving to next node having data " << temp->data << endl;
+				}
+			}
+
+			break;
+
+		case 2:
+
+			cout << "\n\nDo you want to add on the rigth of " << temp->data << "(1/0)" << endl;
+			cin >> choice;
+
+			if (choice == 1) {
+
+				temp->right = newNode;
+				return;
+			}
+			else if (choice == 0) {
+
+				if (temp->right == NULL) {
+
+					cout << "\nTree ends here" << endl;
+				}
+				else {
+
+					temp = temp->right;
+					cout << "\nMoving to next node having data " << temp->data << endl;
+				}
+			}
+			break;
+
+		case 3:
+
+			condition = false;
+			break;
+
+		default:
+			cout << "\n\t Enter Correct option" << endl;
+			break;
+		}
+	}
+}
+
 int binary_tree_basics() {
 
-	Node* root = build_tree(nullptr);
+	/*Node* root = build_tree_recursive(nullptr);*/
+
+	Node* root = new Node(7);
+
+	build_tree(root);
+	build_tree(root);
+	build_tree(root);
+	build_tree(root);
+	build_tree(root);
+	build_tree(root);
+	build_tree(root);
 
 	cout << "Traversing the tree." << endl;
 	
