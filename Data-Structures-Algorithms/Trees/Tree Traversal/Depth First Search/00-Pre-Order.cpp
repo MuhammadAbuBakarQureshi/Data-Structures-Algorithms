19b385071a607cb06cb3eef5445a4a79f35239d0
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-09-07 21:34:13 +0500

    Update 00-Pre-Order.cpp

--- Data-Structures-Algorithms/Trees/Tree Traversal/Depth First Search/00-Pre-Order.cpp
+++ Data-Structures-Algorithms/Trees/Tree Traversal/Depth First Search/00-Pre-Order.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// postO == Post Order
 // DFS == Depth First Search
 
 class Node {
@@ -20,7 +21,7 @@ public:
 	}
 };
 
-void insert_at_tree_DFS(Node* &root, int data) {
+void insert_at_tree_pre_order(Node* &root, int data) {
 
 	Node* newNode = new Node(data);
 
@@ -60,13 +61,13 @@ void insert_at_tree_DFS(Node* &root, int data) {
 	}
 }
 
-void pre_order_DFS_recursive(Node* currentNode){
+void pre_order(Node* currentNode){
 
 	cout << currentNode->data << " ";
 
-	if (currentNode->left)	pre_order_DFS_recursive(currentNode->left);
+	if (currentNode->left)	pre_order(currentNode->left);
 	
-	if (currentNode->right)	pre_order_DFS_recursive(currentNode->right);
+	if (currentNode->right)	pre_order(currentNode->right);
 	
 }
 
@@ -74,15 +75,15 @@ int pre_order_DFS() {
 
 	Node* root = nullptr;
 
-	insert_at_tree_DFS(root, 200);
-	insert_at_tree_DFS(root, 100);
-	insert_at_tree_DFS(root, 300);
-	insert_at_tree_DFS(root, 50);
-	insert_at_tree_DFS(root, 150);
-	insert_at_tree_DFS(root, 250);
-	insert_at_tree_DFS(root, 350);
+	insert_at_tree_pre_order(root, 200);
+	insert_at_tree_pre_order(root, 100);
+	insert_at_tree_pre_order(root, 300);
+	insert_at_tree_pre_order(root, 50);
+	insert_at_tree_pre_order(root, 150);
+	insert_at_tree_pre_order(root, 250);
+	insert_at_tree_pre_order(root, 350);
 
-	pre_order_DFS_recursive(root);
+	pre_order(root);
 	
 	return 0;
 }
