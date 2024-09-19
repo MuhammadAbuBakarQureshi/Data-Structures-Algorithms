1e89f62cb0f004f685882b4ddd28357baae8570d
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-09-19 13:50:45 +0500

    Update Data-Structures-Algorithms.cpp

--- Data-Structures-Algorithms/Data-Structures-Algorithms.cpp
+++ Data-Structures-Algorithms/Data-Structures-Algorithms.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-#include "Header Files/Array/Array-Search.h"
-#include "Header Files/Array/Array-Sort.h"
-#include "Header Files/Array/Array-Functions.h"
-#include "Header Files/Linked Lists/LL.h"
-#include "Header Files/Recursion.h"
-#include "Header Files/Stacks & Queues/Stacks-and-Queues.h"
-#include "Header Files/Hashing/hash.h"
-#include "Header Files/Trees/tree.h"
-#include "Header Files/Vectors/Vectors.h"
-
-using namespace std;
-
-int main() {
-
-	vector_linear_search();
-
-	return 0;
-}
+//#include <iostream>
+//#include "Header Files/Array/Array-Search.h"
+//#include "Header Files/Array/Array-Sort.h"
+//#include "Header Files/Array/Array-Functions.h"
+//#include "Header Files/Linked Lists/LL.h"
+//#include "Header Files/Recursion.h"
+//#include "Header Files/Stacks & Queues/Stacks-and-Queues.h"
+//#include "Header Files/Hashing/hash.h"
+//#include "Header Files/Trees/tree.h"
+//#include "Header Files/Vectors/Vectors.h"
+//
+//using namespace std;
+//
+//int main() {
+//
+//	vector_linear_search();
+//
+//	return 0;
+//}
