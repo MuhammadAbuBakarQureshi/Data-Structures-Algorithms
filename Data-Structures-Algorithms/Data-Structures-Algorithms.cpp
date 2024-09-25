abbe6f2c90822841bba4a5cc3688d9d2da8ae36f
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-09-25 22:24:29 +0500

    Update Data-Structures-Algorithms.cpp

--- Data-Structures-Algorithms/Data-Structures-Algorithms.cpp
+++ Data-Structures-Algorithms/Data-Structures-Algorithms.cpp
@@ -1,21 +1,22 @@
-//#include <iostream>
-//#include "Header Files/Array/Array-Search.h"
-//#include "Header Files/Array/Array-Sort.h"
-//#include "Header Files/Array/Array-Functions.h"
-//#include "Header Files/Array/Sub-Arrays.h"
-//#include "Header Files/Linked Lists/LL.h"
-//#include "Header Files/Recursion.h"
-//#include "Header Files/Stacks & Queues/Stacks-and-Queues.h"
-//#include "Header Files/Hashing/hash.h"
-//#include "Header Files/Trees/tree.h"
-//#include "Header Files/Vectors/Vectors.h"
-//
-//
-//using namespace std;
-//
-//int main() {
-//
-//	kadanes_algorithm();
-//
-//	return 0;
-//}
+#include <iostream>
+#include "Header Files/Array/Array-Questions.h"
+#include "Header Files/Array/Array-Search.h"
+#include "Header Files/Array/Array-Sort.h"
+#include "Header Files/Array/Array-Functions.h"
+#include "Header Files/Array/Sub-Arrays.h"
+#include "Header Files/Linked Lists/LL.h"
+#include "Header Files/Recursion.h"
+#include "Header Files/Stacks & Queues/Stacks-and-Queues.h"
+#include "Header Files/Hashing/hash.h"
+#include "Header Files/Trees/tree.h"
+#include "Header Files/Vectors/Vectors.h"
+
+
+using namespace std;
+
+int main() {
+
+	majority_element();
+	
+	return 0;
+}
